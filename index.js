837e1f466a0d420d13bbd7710a13eded9fad6eda
John Oh <johnoh0317@gmail.com>
2024-05-24 11:44:49 -0700

    added username to 404

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ app.use(express.static(__dirname + "/public"));
 // Catches all 404
 app.get('*', (req, res) => {
     res.status(404);
-    res.render('404', {navLinks});
+    res.render('404', {navLinks, username : req.session.username});
 });
 
 app.listen(port, () => {
