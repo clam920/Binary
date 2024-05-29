0fd02d7cea5de74cadcafe992aba4cc55bf1a519
Mana1422 <manasesvillalobos80@gmail.com>
2024-05-29 00:57:55 -0700

    terms and conditions added to the signup and store in the database

--- public/js/termsDialog.js
+++ public/js/termsDialog.js
@@ -1,9 +1,8 @@
 console.log("terms dialog")
-// function openModal() {
-// }
+
 const modalTerm = document.getElementById('modal');
 
-document.getElementById('openTerms').addEventListener('click', function () {
+document.getElementById('openTermC').addEventListener('click', function () {
     modalTerm.showModal();
 })
 
@@ -12,10 +11,6 @@ function closeModal() {
     document.getElementById('modal').close();
 }
 
-// Close the modal if the user clicks outside of the modal content
-window.onclick = function (event) {
-    var modal = document.getElementById('modal');
-    if (event.target.nodeName === 'DIALOG' && !modal.contains(event.target)) {
-        modal.close();
-    }
-}
+document.getElementById('closeTerms').addEventListener('click', function () {
+    modalTerm.close();
+})
