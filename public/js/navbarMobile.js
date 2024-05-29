868530d05754bf8cc0c0735ad2eb6c0969e25730
Mana1422 <manasesvillalobos80@gmail.com>
2024-05-29 10:05:27 -0700

    Add the information to the terms and condition dialog and minor fix to the mobile navbar

--- public/js/navbarMobile.js
+++ public/js/navbarMobile.js
@@ -1,4 +1,4 @@
-document.getElementById('aProfile').addEventListener('click', ()=>{
+document.getElementById('aProfile').addEventListener('click', function(){
     const dropDown = document.getElementById('dropOpt');
 
     if (dropDown.style.display === 'block') {
@@ -7,12 +7,20 @@ document.getElementById('aProfile').addEventListener('click', ()=>{
         dropDown.style.display = 'block';
       }
 
+           // Highlight the profile link when it's clicked
+     document.querySelectorAll('#linkNav a').forEach(link => {
+      link.classList.remove('active');
+    });
+    this.classList.add('active');
 });
 
 document.addEventListener('click', function(event) {
     var isClickInside = document.getElementById('navMobile').contains(event.target);
     if (!isClickInside) {
       document.getElementById('dropOpt').style.display = 'none';
+      document.getElementById('aProfile').classList.remove('active');
+      window.addEventListener('click', setActiveLink);
+
     }
   });
 
@@ -20,6 +28,7 @@ document.addEventListener('click', function(event) {
     // Remove the 'active' class from all links
     document.querySelectorAll('#linkNav a').forEach(link => {
       link.classList.remove('active');
+
     });
 
   const currentPath = window.location.pathname;
